4986ddb2a0bcfa71ae48b6bc17b483884a9b1bb0
47anjan <anjankarmakar15@gmail.com>
2025-06-29 20:35:30 +0600

    feat: implement delete route for removing favorite recipes

--- src/routes/favorite.js
+++ src/routes/favorite.js
@@ -26,3 +26,31 @@ router.post("/favorite/recipe/save", authorized, async (req, res) => {
     return res.status(401).send({ message: err.message });
   }
 });
+
+router.delete(
+  "/favorite/recipe/remove:recipeId",
+  authorized,
+  async (req, res) => {
+    try {
+      const { recipeId } = req.params;
+
+      if (!recipeId) {
+        res.status(401).send({ message: "Recipe Id cant be empty" });
+      }
+
+      const recipe = await favoriteRecipe.findOne({
+        _id: recipeId,
+      });
+
+      if (!recipe) {
+        throw new Error("Connection does not exist!");
+      }
+
+      const data = await connection.deleteOne();
+
+      res.send(data);
+    } catch (err) {
+      return res.status(401).send({ message: err.message });
+    }
+  }
+);
